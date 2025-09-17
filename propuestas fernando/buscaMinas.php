dc3d36011ee83a06077f4188cf5bc002236596f4
juanxodf <martinpozuelojuan@gmail.com>
2025-09-17 13:07:58 +0200

    Removed the repeated game and updated the minesweeper game.

--- propuestas fernando/buscaMinas.php
+++ propuestas fernando/buscaMinas.php
@@ -1,13 +1,40 @@
 <?php
 // ------------- Juego del buscaminas ------------- //
 
-$tablero = [];
-
+// Crear el tablero
+$filas = 5;
 $minas = rand(2,5);
+function generarTablero($filas, $minas, &$tablero): Array {
+    $tablero = array_fill(0, $filas, $minas);
+    $tablero = array_merge($tablero, $tablero);
+    for ($i = 0; $i < $filas; $i++) {
+        $tablero[$i] = ['-'];
+    }
+    // Colocar minas en posiciones aleatorias
+    $minasColocadas = 0;
+    while ($minasColocadas < $minas) {
+        $filaAleatoria = rand(0, $filas - 1);
+        if ($tablero[$filaAleatoria] !== '*') {
+            $tablero[$filaAleatoria] = '*';
+            $minasColocadas++;
+        }
+    }
+    return $tablero;
+}
+$tableroJuego = generarTablero($filas, $minas, $tablero);
 
-for ($i = 0; $i < $tablero; $i++){
-    $tablero[$i] = "[]";
+function jugar(){
+    global $tableroJuego, $filas;
+    // Mostrar el tablero (para pruebas, en un juego real no se mostraría)
+    for ($i = 0; $i < $filas; $i++) {
+        for ($j = 0; $j < $filas; $j++) {
+            echo $tableroJuego[$i][$j] . " ";
+        }
+        echo "<br>";
+    }
+
 }
 
+jugar();
 
 
